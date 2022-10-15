9d8bab276e159db5f0add2355e04dcdaa69b6ad2
Afeez Adewumi <adewumilanre01@gmail.com>
2022-10-15 17:58:40 -0500

    Adding function that calculates volume of a cuboid

---
+++ js/main.js
@@ -0,0 +1,21 @@
+//write a function that calculates the volume of a couboid
+
+//mysoultion
+
+class Kata {
+  static getVolumeOfCuboid(l, w, h) {
+    return l * w * h;
+  }
+};
+
+//other solutions
+const Kata = {
+  getVolumeOfCuboid(length, width, height) {
+    return length * width * height
+  }
+}
+
+
+const Kata = {
+  getVolumeOfCuboid : (length, width, height) => length * width * height
+}
